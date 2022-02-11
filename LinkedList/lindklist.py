6929b8729d2c2e245eb3511789f6584eb2c24ae5
unknown <averma2@student.nitw.ac.in>
2022-02-11 17:34:36 +0530

    LinkdList programs to print_elements, to_insert_at_tail, to_insert_at_head, to_insert_between_the_nodes

--- LinkedList/lindklist.py
+++ LinkedList/lindklist.py
@@ -30,24 +30,58 @@ class Node(object):
 
 
     def print_list(self):
-        '''Print the list element'''
-        
+        '''Print the list element
+        using genarator 
+        '''
         while self is not None:
             yield self.data
             self = self.next
 
+    def print_element_of_node(self):
+        return self.data
 
 
+    def insert_node_at_tail(self, head):
+        '''insert node at tail'''
+        while head.next is not None:
+            head = head.next
+
+        head.next = self
+        self.next = None
+
+
+    def insert_node_at_head(self, head):
+        '''insert node at head'''
+        self.next = head
+
+
+    def inset_between_the_nodes(self, first_node, second_node):
+        ''' insert between the first and second node'''
+
+        first_node.next = self
+        self.next = second_node
+
+        
+        
 
 a = Node(5)
 b = Node(7)
 c = Node(9)
-
+d = Node(12)
+e = Node(11)
+f = Node(3)
 a.next = b
 b.next = c
 c.next = None
 
-value = a.print_list()
+
+d.insert_node_at_tail(a)
+e.insert_node_at_head(a)
+f.inset_between_the_nodes(b,c)
+value = e.print_list()
+print(next(value))
+print(next(value))
+print(next(value))
 print(next(value))
 print(next(value))
 print(next(value))
@@ -55,26 +89,5 @@ print(next(value))
 
 
 
-# s = sum_of_list(a)
-# print(f'sum of nodes are {s}')
-
-# a = Node("a")
-# b = Node("c")
-# c = Node("g")
-
-# a.next = b
-# b.next = c
-# c.next = None
-# c = create_list(a)
-# print(c)
 
 
-
-# def sum_of_list(node):
-#     '''sum the nodes and output the result'''
-#     sum = 0
-#     while node is not None:
-#         sum+=node.data
-#         node=node.next
-
-#     return sum
