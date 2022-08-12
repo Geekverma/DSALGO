59afcdc6f4c1e267b2fd51533eca93e8da379ddc
unknown <averma2@student.nitw.ac.in>
2022-08-12 10:34:52 +0530

    add comment

--- Arrays/merge-array.py
+++ Arrays/merge-array.py
@@ -4,6 +4,7 @@ from typing import List
 def merge(l1: List, l2: List, m:int, n:int) -> List:
     i,j,k=0,0,0
     temp = []
+    #fill temp by comapring each index of l1 and l2 element.
     while i<m and j<n:
         if l1[i] <= l2[j]:
             # print(i,j,temp[0],l1[i])
